5cfdc2ac8c1e2b4c2b7bdc44e0c53aa6ddededd4
Alex White <alexlloydwhite@gmail.com>
2021-07-28 12:15:32 -0500

    all tests passing

--- src/Counter/Counter.jsx
+++ src/Counter/Counter.jsx
@@ -1,14 +1,21 @@
 import { useState } from 'react';
+import './Counter.css';
 
 export default function Counter() {
     const [counterValue, setCounterValue] = useState(0);
+    const [inputValue, setInputValue] = useState(1);
 
   return (
     <div>
         <h2 data-testid="header">My Counter</h2>
         <h2 data-testid="counter">{counterValue}</h2>
         <button data-testid="subtract-btn">-</button>
-        
+        <input 
+            data-testid="input" 
+            type="number" 
+            value={inputValue}
+            className="text-center"
+        />
         <button data-testid="add-btn">+</button>
     </div>
   );
