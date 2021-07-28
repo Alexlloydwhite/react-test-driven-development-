d4d6795446c7b96070b1bac8e2d3b2d4d312fbf1
Alex White <alexlloydwhite@gmail.com>
2021-07-28 12:01:13 -0500

    destructering getByTestId

--- src/Counter/__test__/Counter.test.js
+++ src/Counter/__test__/Counter.test.js
@@ -4,8 +4,8 @@ import { render } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 
 test("header renders with correct test", () => {
-    const component = render(<Counter />);
-    const headerEl = component.getByTestId('header');
+    const { getByTestId } = render(<Counter />);
+    const headerEl = getByTestId('header');
 
     expect(headerEl.textContent).toBe('My Counter');
 });
